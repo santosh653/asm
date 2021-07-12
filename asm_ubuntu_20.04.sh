a0ae4a817528aa36a39cf94f8118d720a416a906
santosh653 <70637961+santosh653@users.noreply.github.com>
2021-07-12 11:28:58 +0530

    Create asm_ubuntu_20.04.sh
    add build script.

---
+++ asm_ubuntu_20.04.sh
@@ -0,0 +1,52 @@
+# ----------------------------------------------------------------------------
+#
+# Package           : asm
+# Version           : ASM_9_1
+# Source repo       : https://github.com/ljvblfz/asm
+# Tested on         : ubuntu_20.04
+# Script License    : Apache License, Version 2 or later
+# Maintainer        : Santosh Kulkarni <santoshkulkarni70@gmail.com> / Priya Seth<sethp@us.ibm.com>
+#
+# Disclaimer        : This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+
+export REPO=https://github.com/ljvblfz/asm
+
+if [ -z "$1" ]; then
+  export VERSION="ASM_9_1"
+else
+  export VERSION="$1"
+fi
+
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk wget git -y
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-ppc64el/
+sudo apt install  -y maven
+mvn -version
+
+if [ -d "asm" ] ; then
+  rm -rf asm
+fi
+
+git clone ${REPO}
+
+## Build and asm
+cd asm
+git checkout ${VERSION}
+ret=$?
+
+if [ $ret -eq 0 ] ; then
+  echo "$Version found to checkout "
+else
+  echo "$Version not found "
+  exit
+fi
+
+mvn clean install
